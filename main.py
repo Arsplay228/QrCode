804208591b5fc980834500bcf279e4cec5cf52fa
Arsplay <ok388615@gmail.com>
2025-05-03 12:41:13 +0300

    feat: QrCode

---
+++ main.py
@@ -0,0 +1,15 @@
+import qrcode
+import os
+from PIL import Image
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("some_file.png")
+os.startfile("some_file.png")
